b5a306524053b99933f551eb6a9bb5e9726aeeab
Xiaoke Li <michael@thinknear.com>
2011-07-22 19:04:11 -0700

    Add User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SampleApp::Application.routes.draw do
   get "users/new"
+  
+  resources :users
 
-   match '/signup',  :to => 'users#new'
+  match '/signup',  :to => 'users#new'
 
-   match '/contact', :to => 'pages#contact'
-   match '/about',   :to => 'pages#about'
-   match '/help',    :to => 'pages#help'
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
 
    root :to => 'pages#home'
      
